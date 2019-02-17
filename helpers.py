4a18fbfdabeab20d2942257a0cfd27ebad374d90
Denis Burenko <burenko@yandex-team.ru>
2019-02-17 16:31:47 +0300

    pip8 fix

--- helpers.py
+++ helpers.py
@@ -3,11 +3,13 @@ import tarfile
 from six.moves import urllib
 import pandas as pd
 
+
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
-def fetch_housing_data(housing_url =  HOUSING_URL, housing_path = HOUSING_PATH):
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=ыHOUSING_PATH):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
     tgz_path = os.path.join(housing_path, "housing.tgz")
@@ -16,7 +18,7 @@ def fetch_housing_data(housing_url =  HOUSING_URL, housing_path = HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
-def load_housing_data(housing_path =  HOUSING_PATH):
+
+def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
-
